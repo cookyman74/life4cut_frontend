a5c667820ab81eee1cf2401a1323df5cd2ba4986
junghojang <cookyman@gmail.com>
2024-11-30 00:53:43 +0900

    canvasEditor 화면고정기능 수정.(조금도 수정할 필요가 있음) todo: - 스티커 삭제 기능

--- src/components/Editor/hooks/useInteractionMode.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-
-export type InteractionMode = 'sticker' | 'text' | null;
-
-export const useInteractionMode = () => {
-    const [mode, setMode] = useState<InteractionMode>(null);
-    return { mode, setMode };
-};
